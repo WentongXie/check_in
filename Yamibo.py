c250beeb24e8b7167ed834402e662321e8305a4b
xwt <93423084+WentongXie@users.noreply.github.com>
2024-05-01 10:48:51 +0800

    Update Yamibo.py

--- Yamibo.py
+++ Yamibo.py
@@ -5,20 +5,19 @@ import logging
 import traceback
 from bs4 import BeautifulSoup
 from urllib import parse
-import cloudscraper
-import http.cookies
 
 header = {
-    "User-Agent": r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "User-Agent": r"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
     "Accept-Language": r"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept": r"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept": r"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
 }
 
 def YamiboLogin(session, account, password):
     login_info_url = r"https://bbs.yamibo.com/member.php?mod=logging&action=login&mobile=2"
-    login_info_rsp = session.get(login_info_url, headers=header)
+    login_info_rsp = session.get(login_info_url)
     login_info_bs = BeautifulSoup(login_info_rsp.text, "html.parser")
-    form_hash = login_info_bs.find("input", id="formhash")
+    form_hash = login_info_bs.find("input", attrs={"name": "formhash"})
     assert form_hash != None and form_hash.get("value") != None, login_info_rsp.text
     form_hash = form_hash.get("value")
     login_url = login_info_bs.find("form", id="loginform")
@@ -26,8 +25,7 @@ def YamiboLogin(session, account, password):
     login_url = login_url.get("action")
     login_url = parse.urljoin("https://bbs.yamibo.com/", login_url)
     logging.info("login_hash: {}, form_hash:{}".format(login_url, form_hash))
-    login = session.post(login_url, headers=header,
-                         data=YamiboGetLoginData(account, password, form_hash))
+    login = session.post(login_url, data=YamiboGetLoginData(account, password, form_hash))
     uid = re.search(r'''uid=(\d+)&''', login.text)
     assert uid != None, login.text
     uid = uid.group(1)
@@ -51,7 +49,7 @@ def YamiboGetLoginData(account, password, form_hash):
 
 def YamiboLogout(session, uid):
     rsp = session.get(
-        "https://bbs.yamibo.com/home.php?mod=space&uid={}&do=profile&mycenter=1&mobile=2".format(uid), headers=header)
+        "https://bbs.yamibo.com/home.php?mod=space&uid={}&do=profile&mycenter=1&mobile=2".format(uid))
     home_bs = BeautifulSoup(rsp.text, "html.parser")
     logout_url = home_bs.find("a", class_="pn")
     assert logout_url != None and logout_url.get("href") != None, rsp.text
@@ -68,42 +66,38 @@ def YamiboSign(session):
     assert sign_url != None and sign_url.get("href") != None, rsp.text
     sign_url = sign_url.get("href")
     sign_url = parse.urljoin("https://bbs.yamibo.com/", sign_url)
-    sign_rsp = session.get(sign_url, headers=header)
-    assert sign_rsp.text.find("打卡成功") != -1, sign_rsp.text
+    sign_rsp = session.get(sign_url)
+    assert sign_rsp.text.find("打卡成功") != -1 or sign_rsp.text.find("已经打过卡") != -1, sign_rsp.text
     logging.debug(sign_rsp.text)
 
 
-def YamiboCheck(cookie):
+def YamiboCheck(account, password):
     try:
         with requests.Session() as s:
             s.headers.update(header)
-            cookie = http.cookies.SimpleCookie(cookie)
-            cookie_jar = requests.cookies.RequestsCookieJar()
-            cookie_jar.update(cookie)
-            s.cookies.update(cookie_jar)
-            #uid = YamiboLogin(s, account, password)
-            YamiboLogUserInfo(s)
+            uid = YamiboLogin(s, account, password)
+            YamiboLogUserInfo(s, uid)
             YamiboSign(s)
-            YamiboLogUserInfo(s)
-            #YamiboLogout(s, uid)
+            YamiboLogUserInfo(s, uid)
+            YamiboLogout(s, uid)
             logging.info("YamiboCheck finish.")
     except Exception as e:
         logging.error("Exception: %s", traceback.format_exc())
         PushMessage("yamibo check failed.", "yamibo check failed.")
 
 
-def YamiboLogUserInfo(session):
-    rsp = session.get("https://bbs.yamibo.com/home.php?mod=spacecp&ac=credit")
+def YamiboLogUserInfo(session, uid):
+    rsp = session.post("https://bbs.yamibo.com/home.php?mod=space&uid={}&do=profile&mycenter=1&mobile=2".format(uid))
     home_bs = BeautifulSoup(rsp.text, "html.parser")
     logging.debug(rsp.text)
-    info = home_bs.find("ul", class_="creditl mtm bbda cl")
+    info = home_bs.find("div", class_="user_box cl")
     assert info is not None, rsp.text
     logging.info(info.get_text())
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    YamiboCheck("cookie")
+    YamiboCheck("account", "password")
     pass
 
 
