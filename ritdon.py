cf5ff50e201a774caea72d72beb12a9294b5b7c8
xwt <93423084+WentongXie@users.noreply.github.com>
2025-02-14 22:05:09 +0800

    add formhash field

--- ritdon.py
+++ ritdon.py
@@ -1,20 +1,27 @@
 import requests, logging, time, traceback
 from Push import PushMessage
+from bs4 import BeautifulSoup
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
 }
 
 def ritdon_login(session, account, password):
-    url = "http://ritdon.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
-    payload = "fastloginfield=username&username={}&password={}&quickforward=yes&handlekey=ls".format(account, password)
+    hash_rsp = session.get("https://ritdon.com/")
+    hash_bs = BeautifulSoup(hash_rsp.text, "html.parser")
+    form_hash = hash_bs.find("input", {"name": "formhash"})
+    assert form_hash != None and form_hash.get("value") != None, hash_rsp.text
+    formhash = form_hash.get("value")
+    url = "https://ritdon.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
+    payload = "formhash={}&username={}&password={}&quickforward=yes&handlekey=ls".format(formhash, account, password)
     rsp = session.post(url, headers = {"Content-Type":"application/x-www-form-urlencoded"},data = payload)
-    assert "window.location.href='http://ritdon.com/" in rsp.text, "login: {}, req header: {}, rsp header: {}".format(rsp.text,  rsp.request.headers, rsp.headers)
+    logging.debug("login rsp: {}".format(rsp.text))
+    assert "window.location.href='https://ritdon.com/" in rsp.text, "login: {}, req header: {}, rsp header: {}".format(rsp.text,  rsp.request.headers, rsp.headers)
     logging.debug("login: {}, req header: {}, rsp header: {}".format(rsp.text,  rsp.request.headers, rsp.headers))
     pass
 
 def ritdon_user_info(session):
-    url = "http://ritdon.com/home.php?mod=spacecp&ac=credit&showcredit=1&inajax=1&ajaxtarget=extcreditmenu_menu"
+    url = "https://ritdon.com/home.php?mod=spacecp&ac=credit&showcredit=1&inajax=1&ajaxtarget=extcreditmenu_menu"
     rsp = session.get(url)
     body = rsp.text.encode(rsp.encoding).decode(rsp.apparent_encoding)
     assert "金币" in body, "no login: {}".format(body)
@@ -22,9 +29,9 @@ def ritdon_user_info(session):
     pass
 
 def ritdon_sign(session):
-    urls = ["http://ritdon.com/",
-        "http://ritdon.com/home.php?mod=spacecp&ac=pm&op=checknewpm&rand="+str(int(time.time())),
-        "http://ritdon.com/home.php?mod=misc&ac=sendmail&rand="+str(int(time.time()))
+    urls = ["https://ritdon.com/",
+        "https://ritdon.com/home.php?mod=spacecp&ac=pm&op=checknewpm&rand="+str(int(time.time())),
+        "https://ritdon.com/home.php?mod=misc&ac=sendmail&rand="+str(int(time.time()))
     ]
     for i in urls:
         logging.info(i)
